2f32fc41c16a6c50b052c3893eaec95db40ce822
Philip Medlin <phmedlin@gmail.com>
2021-09-01 14:41:09 -0600

    Feat: create room class with methods from prompt

---
+++ lib/room.rb
@@ -0,0 +1,27 @@
+class Room
+  attr_reader :category,
+              :length,
+              :width
+  attr_accessor   :is_painted
+              #:area
+
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
+    @is_painted = false
+  end
+
+  def area
+    @length * @width.to_i
+  end
+
+  def is_painted?
+    @is_painted
+  end
+
+  def paint
+    @is_painted = true
+  end
+
+end
