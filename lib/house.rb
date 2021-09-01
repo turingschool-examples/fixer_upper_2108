ec1c7b0f6de60f80d0a6dc23857d8b829977ad43
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-01 14:53:46 -0600

    Create House class

---
+++ lib/house.rb
@@ -0,0 +1,11 @@
+class House
+
+  attr_reader :price, :address, :rooms
+
+  def initialize(price, address)
+    @price = price.gsub("$",'').to_i
+    @address = address
+    @rooms = []
+  end
+
+end
