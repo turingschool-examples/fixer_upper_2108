1f880342df5eb3c0b766bf5dc86198a7fc2e58b5
Micha Bernhard <mib70@pitt.edu>
2021-09-01 14:37:43 -0600

    Feat - Add initialize method

---
+++ lib/house.rb
@@ -0,0 +1,14 @@
+require './lib/room'
+
+class House
+  attr_reader :price,
+              :address,
+              :rooms
+
+  def initialize(price, address)
+    @price = price
+    @address = address
+    @rooms = []
+  end
+
+end
