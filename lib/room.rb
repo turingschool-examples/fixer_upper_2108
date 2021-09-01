3800bf53f7d473c11e178387d8069b1195a4be16
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-01 14:34:33 -0600

    Create is_painted? instance method

--- lib/room.rb
+++ lib/room.rb
@@ -2,14 +2,19 @@ class Room
 
   attr_reader :category
 
-  def initialize(category, length, width)
+  def initialize(category, length, width, is_painted = false)
     @category = category
     @length = length
     @width = width
+    @is_painted = is_painted
   end
 
   def area
     @length*(@width.to_i)
+  end
+
+  def is_painted?
+    @is_painted
   end
 
 end
