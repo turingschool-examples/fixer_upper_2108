a3c63ae7b728485298e32144ef7269861a37f829
Micha Bernhard <mib70@pitt.edu>
2021-09-01 14:30:30 -0600

    Test - Add tests for is_painted? and paint

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -20,13 +20,36 @@ describe 'room' do
 
       expect(room.width).to eq("13")
     end
+
+    it 'is not painted by default' do
+      room = Room.new(:bedroom, 10, "13")
+
+      expect(room.is_painted).to eq(false)
+    end
   end
 
-  describe 'area' do
+  describe '#area' do
     it 'calculates the area of the room' do
       room = Room.new(:bedroom, 10, "13")
 
       expect(room.area).to eq(130)
+    end
+  end
+
+  describe '#is_painted?' do
+    it 'returns false by default for is_painted' do
+      room = Room.new(:bedroom, 10, "13")
+
+      expect(room.is_painted?).to eq(false)
+    end
+  end
+
+  describe '#paint' do
+    it 'makes is_painted return true' do
+      room = Room.new(:bedroom, 10, "13")
+      room.paint
+
+      expect(room.is_painted?).to eq(true)
     end
   end
 end
