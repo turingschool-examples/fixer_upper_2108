c52b7e8649c9fb903573ea571ebe84fd99d75fbc
Micha Bernhard <mib70@pitt.edu>
2021-09-01 14:42:24 -0600

    Test - Add test for add_room method

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -22,4 +22,16 @@ describe 'house' do
       expect(house.rooms).to eq([])
     end
   end
+
+  describe '#add_room' do
+    it 'adds rooms to the house' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      house.add_room(room_1)
+      house.add_room(room_2)
+
+      expect(house.rooms.count).to eq(2)
+    end
+  end
 end
