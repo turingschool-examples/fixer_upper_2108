3c98330aaef9682f883c5a2ee626eda4467fefdc
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-01 14:35:22 -0600

    Test - Add tests for init, add_room, market_price, and category

---
+++ spec/house_spec.rb
@@ -0,0 +1,83 @@
+require './lib/room'
+require './lib/house'
+require 'rspec'
+
+describe 'House' do
+  describe '#initialize' do
+    it 'creates a new house with a price' do
+      house = House.new("$400000", "123 sugar lane")
+
+      expect(house.price).to eq("$400000")
+    end
+
+    it 'creates a new house with an address' do
+      house = House.new("$400000", "123 sugar lane")
+
+      expect(house.address).to eq("123 sugar lane")
+    end
+
+    it 'creates a new house with an empty array of rooms' do
+      house = House.new("$400000", "123 sugar lane")
+
+      expect(house.rooms).to eq([])
+    end
+  end
+  describe '#add_room' do
+    it 'adds rooms to the house' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      house.add_room(room_1)
+      house.add_room(room_2)
+
+      expect(house.rooms).to eq([room_1, room_2])
+    end
+  end
+  describe '#above_market_price' do
+    it 'checks if a house is above market price' do
+      house = House.new("$400000", "123 sugar lane")
+
+      expect(house.above_market_price?).to eq(false)
+    end
+    it 'checks if a house is above market price' do
+      house = House.new("$500000", "123 sugar lane")
+
+      expect(house.above_market_price?).to eq(false)
+    end
+    it 'checks if a house is above market price' do
+      house = House.new("$600000", "123 sugar lane")
+
+      expect(house.above_market_price?).to eq(true)
+    end
+  end
+  describe '#rooms_from_category' do
+    it 'shows all rooms from a given category' do
+      house = House.new("$600000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+
+      expect(house.rooms_from_category(:bedroom)).to eq([room_1, room_2])
+    end
+
+    it 'shows all rooms from a given category' do
+      house = House.new("$600000", "123 sugar lane")
+      house = House.new("$600000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+
+      expect(house.rooms_from_category(:basement)).to eq([room_4])
+    end
+  end
+end
