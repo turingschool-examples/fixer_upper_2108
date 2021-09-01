176932a14c3dcf29714caa665afd526eec0a3a6a
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-01 14:34:47 -0600

    Feat - Add market price and rooms_from_category methods

--- lib/house.rb
+++ lib/house.rb
@@ -12,4 +12,17 @@ class House
   def add_room(room)
     @rooms << room
   end
+
+  def above_market_price?
+    house_price = @price.slice(1..).to_i
+    if house_price > 500000
+      true
+    else
+      false
+    end
+  end
+
+  def rooms_from_category(category)
+    @rooms.find_all { |room| room.category == category }
+  end
 end
