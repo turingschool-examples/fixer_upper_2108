f22c704e694b712fa58ac28b01030b3072aba523
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-01 15:36:11 -0600

    Test - Add tests for rooms by category method

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -124,4 +124,19 @@ describe 'House' do
       expect(house.price_per_square_foot).to eq(210.53)
     end
   end
+  describe '#rooms_by_category' do
+    it 'returns all rooms organized by category' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_4)
+      house.add_room(room_1)
+      house.add_room(room_3)
+      house.add_room(room_2)
+
+      expect(house.rooms_by_category).to eq({bedroom: [room_1, room_2], living_room: [room_3], basement: [room_4]})
+    end
+  end
 end
