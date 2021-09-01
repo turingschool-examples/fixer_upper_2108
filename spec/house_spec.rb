9c407f952b415315b2b72194ad8bb2c509349713
Andrew Smith <c13andrew.smith@gmail.com>
2021-09-01 14:11:39 -0700

    #rooms_from_category

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -34,12 +34,11 @@ RSpec.describe House do
 
   it 'can be above market average' do
     house = House.new("$400000", "123 sugar lane")
-    expect(house.above_market_average).to eq(false)
+    expect(house.above_market_average?).to eq(false)
   end
 
   it 'can return rooms from category' do
     house = House.new("$400000", "123 sugar lane")
-    expect(house.above_market_average).to eq(false)
     room_1 = Room.new(:bedroom, 10, '13')
     room_2 = Room.new(:bedroom, 11, '15')
     room_3 = Room.new(:living_room, 25, '15')
@@ -48,7 +47,21 @@ RSpec.describe House do
     house.add_room(room_2)
     house.add_room(room_3)
     house.add_room(room_4)
-    expect(house.rooms_from_category(:bedroom)).to eq(room_1, room_2)
+    expect(house.rooms_from_category(:bedroom)).to eq([room_1, room_2])
+    expect(house.rooms_from_category(:basement)).to eq([room_4])
+  end
+
+  it 'can return the area of the house' do
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    expect(house.area).to eq(1900)
   end
 
 
