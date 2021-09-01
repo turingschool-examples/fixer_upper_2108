aa8539d4858fb576af46ce0c4939474e71ee5806
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-01 15:18:05 -0600

    Test: create #rooms_from_category test

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -32,4 +32,18 @@ describe House do
     end
   end
 
+  describe "#rooms_from_category" do
+    it "returns and array of room objects matching specified category" do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+
+      expect(house.room_from_category(:bedroom)).to eq([room_1, room_2])
+    end
+  end
+
 end
