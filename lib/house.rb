2dbc3c301c1b43a77c5651f1c12e4933f7fdb64e
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-01 15:27:18 -0600

    Feat - Add methods for area, details, and price_per_square_foot

--- lib/house.rb
+++ lib/house.rb
@@ -25,4 +25,23 @@ class House
   def rooms_from_category(category)
     @rooms.find_all { |room| room.category == category }
   end
+
+  def area
+    house_area = 0
+    @rooms.each do |room|
+      house_area += room.area.to_i
+    end
+    return house_area
+  end
+
+  def details
+    details = {
+      "price" => @price.slice(1..),
+      "address" => @address
+    }
+  end
+
+  def price_per_square_foot
+    (@price.slice(1..).to_f / area).round(2)
+  end
 end
