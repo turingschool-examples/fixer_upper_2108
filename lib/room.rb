ee12e97b2d3410f2792f78af7386ddb982404c59
Karan Mehta <karanm645@gmail.com>
2021-09-01 16:56:09 -0500

    "Initialize + finish room"

---
+++ lib/room.rb
@@ -0,0 +1,25 @@
+class Room
+  attr_reader :category,
+              :length,
+              :width,
+              :is_painted
+
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
+    @is_painted = false
+  end
+
+  def area
+    @width.to_i * @length
+  end
+
+  def is_painted?
+    @is_painted
+  end
+
+  def paint
+    @is_painted = true
+  end
+end
