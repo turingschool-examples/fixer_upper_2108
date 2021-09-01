19cfac42adcd5dcf7b89c1b65c613a5a2b5b8d9e
Andrew Smith <c13andrew.smith@gmail.com>
2021-09-01 13:20:49 -0700

    #area test

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -1,11 +1,21 @@
-equire './lib/room'
+require './lib/room'
 
 RSpec.describe Room do
 
   it 'exists' do
 
   room = Room.new(:bedroom, 10, "13")
+  expect(room).to be_instance_of(Room)
+  end
 
+  it 'has a category' do
+    room = Room.new(:bedroom, 10, "13")
+    expect(room.category).to eq(:bedroom)
+  end
+
+  it 'returns area' do
+    room = Room.new(:bedroom, 10, "13")
+    expect(room.area).to eq(130)
   end
 
 end
