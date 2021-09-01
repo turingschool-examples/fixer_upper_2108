5a55b28e28fb90ed6e79068b9b9886d3cf9bbeb6
Andrew Smith <c13andrew.smith@gmail.com>
2021-09-01 14:48:36 -0700

    add: describe statements

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -2,8 +2,8 @@ require './lib/room'
 
 RSpec.describe Room do
 
+describe '#initialize' do
   it 'exists' do
-
   room = Room.new(:bedroom, 10, "13")
   expect(room).to be_instance_of(Room)
   end
@@ -22,22 +22,34 @@ RSpec.describe Room do
     room = Room.new(:bedroom, 10, "13")
     expect(room.width).to eq("13")
   end
+end
 
-  it 'returns area' do
-    room = Room.new(:bedroom, 10, "13")
-    expect(room.area).to eq(130)
-  end
+describe '#area' do
+    it 'returns area' do
+      room = Room.new(:bedroom, 10, "13")
+      expect(room.area).to eq(130)
+    end
 
+    it 'can return a different area' do
+      room = Room.new(:bedroom, 9, "10")
+      expect(room.area).to eq(90)
+    end
+end
+
+describe '#is_painted?' do
   it 'is not painted by default' do
     room = Room.new(:bedroom, 10, "13")
     expect(room.is_painted?).to eq(false)
   end
+end
 
+describe '#paint' do
   it 'can become painted' do
     room = Room.new(:bedroom, 10, "13")
     expect(room.is_painted?).to eq(false)
     room.paint
     expect(room.is_painted?).to eq(true)
   end
+end
 
 end
