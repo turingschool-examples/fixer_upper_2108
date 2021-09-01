b04232547ffea0bc1f05ff12e6623fec9774223c
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-01 15:53:17 -0600

    Fix - Formatting for house_spec, commented out rooms_by_area test

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -22,6 +22,7 @@ describe 'House' do
       expect(house.rooms).to eq([])
     end
   end
+
   describe '#add_room' do
     it 'adds rooms to the house' do
       house = House.new("$400000", "123 sugar lane")
@@ -33,6 +34,7 @@ describe 'House' do
       expect(house.rooms).to eq([room_1, room_2])
     end
   end
+
   describe '#above_market_price' do
     it 'checks if a house is above market price' do
       house = House.new("$400000", "123 sugar lane")
@@ -50,6 +52,7 @@ describe 'House' do
       expect(house.above_market_price?).to eq(true)
     end
   end
+
   describe '#rooms_from_category' do
     it 'shows all rooms from a given category' do
       house = House.new("$600000", "123 sugar lane")
@@ -79,6 +82,7 @@ describe 'House' do
       expect(house.rooms_from_category(:basement)).to eq([room_4])
     end
   end
+
   describe '#area' do
     it 'finds the combined area of all rooms in the house' do
       house = House.new("$600000", "123 sugar lane")
@@ -94,6 +98,7 @@ describe 'House' do
       expect(house.area).to eq(1900)
     end
   end
+
   describe '#details' do
     it 'returns the price and address of the house in hash format' do
       house = House.new("$600000", "123 sugar lane")
@@ -109,6 +114,7 @@ describe 'House' do
       expect(house.details).to eq({"price" => "600000", "address" => "123 sugar lane"})
     end
   end
+
   describe '#price_per_square_foot' do
     it 'returns the price by square foot' do
       house = House.new("$400000", "123 sugar lane")
@@ -124,6 +130,23 @@ describe 'House' do
       expect(house.price_per_square_foot).to eq(210.53)
     end
   end
+
+  # describe '#rooms_sorted_by_area' do
+  #   it 'sorts rooms by largest to smallest area' do
+  #     house = House.new("$400000", "123 sugar lane")
+  #     room_1 = Room.new(:bedroom, 10, '13')
+  #     room_2 = Room.new(:bedroom, 11, '15')
+  #     room_3 = Room.new(:living_room, 25, '15')
+  #     room_4 = Room.new(:basement, 30, '41')
+  #     house.add_room(room_4)
+  #     house.add_room(room_1)
+  #     house.add_room(room_3)
+  #     house.add_room(room_2)
+  #
+  #     expect(house.rooms_sorted_by_area).to eq([room_4, room_3, room_2, room_1])
+  #   end
+  end
+
   describe '#rooms_by_category' do
     it 'returns all rooms organized by category' do
       house = House.new("$400000", "123 sugar lane")
