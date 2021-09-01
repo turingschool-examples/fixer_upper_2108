6c0a64ae9771694b6dc917bd2c5983469e177b80
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-01 14:24:19 -0600

    Test: edit #initialize test

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -3,12 +3,12 @@ require './lib/room'
 
 describe Room do
 
-  describe '#initialize' do
+  describe "#initialize" do
     it "creates an instance of Room and has readable attribute category" do
-      room = Room.new(:bedroom, 10, "13")
+       room = Room.new(:bedroom, 10, "13")
 
-      expect(room).to be_an_instance_of(Room)
-      expect(room.category).to eq(:bedroom)
+       expect(room).to be_an_instance_of(Room)
+       expect(room.category).to eq(:bedroom)
     end
   end
 
