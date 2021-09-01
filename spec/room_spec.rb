f0d2c0bd8aabee4ac23360a93a5c8a3cfeb9b662
Billy Frey <bfrey16@gmail.com>
2021-09-01 15:08:03 -0600

    feat: add room class test file

---
+++ spec/room_spec.rb
@@ -0,0 +1,33 @@
+require './lib/room.rb'
+
+RSpec.describe Room do
+  it "exists" do
+    room = Room.new(:bedroom, 10, "13")
+
+    expect(room).to be_an_instance_of(Room)
+  end
+
+  it "can tell you the category of room" do
+    room = Room.new(:bedroom, 10, "13")
+
+    expect(room.category).to eq(:bedroom)
+  end
+
+  it "can tell you the area of the room" do
+    room = Room.new(:bedroom, 10, "13")
+
+    expect(room.area).to eq(130)
+  end
+
+  it "can tell you if its painted or not" do
+    room = Room.new(:bedroom, 10, "13")
+
+    expect(room.is_painted).to be(false)
+  end
+
+  it "can paint a room" do
+    room = Room.new(:bedroom, 10, "13")
+    room.paint
+    expect(room.is_painted).to eq(true)
+  end
+end
